78e3b2d6029cda9f5344bc2680a34c12b3962a30
supertramps <alexander.liljedahl@live.se>
2020-10-26 14:56:35 +0100

    Added javascript

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-var input = document.getElementById("text-input");
+function getAnswer() {
+  var input = document.getElementById("answer").value;
+
+  if (input === "inspect") {
+    document.getElementById("game-start").classList.toggle(hidden);
+    document.getElementById("inspect").classList.toggle(hidden);
+    } else if (input == 'exit') {
+    document.getElementById("game-start").classList.toggle(hidden);
+    document.getElementById("exit-room").classList.toggle(hidden);
+    } else { document.getElementById("error")}.innerHTML = "You tried doing ++answer++ but nothing happened." }
+}
