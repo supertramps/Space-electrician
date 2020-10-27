4fab8a4fa46b4131406eaf068bf95b8e792322de
Alexander Liljedahl <alexander.liljedahl@live.se>
2020-10-27 14:27:10 +0100

    Updated JS. Added array to confirm user inputs.

--- game.js
+++ game.js
@@ -1,20 +1,24 @@
+let userInputs = [];
+
 /**
- * @function This function clears the input field when called.
- * @return null
+ * clears the input field when called.
+ *
  */
 function clearField() {
   document.getElementById("answer").value = "";
 }
 /**
- * @function This function clears the error message when called.
- * @return null
+ * clears the error message when called.
+ *
  */
 function clearError() {
   document.getElementById("error").innerHTML = "";
 }
 /**
- * @function This function reads user input and presents outcome.
+ * reads user input and presents outcome. Uses classList.toggle to show or hide elements (in our case <p> tags) depending on user input.
+ * Also confirms input with the array userInputs so the user can't enter commands that he/she isn't allowed to at a certain state of the game.
  */
+
 function getAnswer() {
   if (document.getElementById("error").innerHTML.startsWith("You tried")) {
     clearError();
@@ -25,46 +29,61 @@ function getAnswer() {
   if (input === "start") {
     document.getElementById("intro").classList.toggle("hidden");
     document.getElementById("game-start").classList.toggle("hidden");
-  } else if (input == "inspect") {
+    userInputs.push(input);
+  } else if (input == "inspect" && userInputs.includes("start")) {
     document.getElementById("game-start").classList.toggle("hidden");
     document.getElementById("inspect").classList.toggle("hidden");
-  } else if (input == "yes") {
+    userInputs.push(input);
+  } else if (input == "yes" && userInputs.includes("inspect")) {
     document.getElementById("inspect").classList.toggle("hidden");
     document.getElementById("use-hat").classList.toggle("hidden");
-  } else if (input == "no") {
+    userInputs.push(input);
+  } else if (input == "no" && userInputs.includes("inspect")) {
     document.getElementById("inspect").classList.toggle("hidden");
     document.getElementById("leave-room-no-hat").classList.toggle("hidden");
-  } else if (input == "leave") {
+    userInputs.push(input);
+  } else if (input == "leave" && userInputs.includes("yes")) {
     document.getElementById("use-hat").classList.toggle("hidden");
     document.getElementById("leave-room-hat").classList.toggle("hidden");
-  } else if (input == "leave") {
-    document.getElementById("use-hat").classList.toggle("hidden");
-    document.getElementById("leave-room-hat").classList.toggle("hidden");
-  } else if (input == "east") {
-    document.getElementById("leave-room-hat").classList.toggle("hidden");
-    document.getElementById("left").classList.toggle("hidden");
-  } else if (input == "west") {
-    document.getElementById("leave-room-hat").classList.toggle("hidden");
-    document.getElementById("right").classList.toggle("hidden");
-  } else if (input == "exit") {
+  } else if (input == "exit" && userInputs.includes("start")) {
     document.getElementById("game-start").classList.toggle("hidden");
     document.getElementById("exit-room").classList.toggle("hidden");
-  } else if (input == "cafeteria") {
+    userInputs.push(input);
+  } else if (input == "east" && userInputs.includes("yes")) {
+    document.getElementById("leave-room-hat").classList.toggle("hidden");
+    document.getElementById("left").classList.toggle("hidden");
+    userInputs.push(input);
+  } else if (input == "west" && userInputs.includes("yes")) {
+    document.getElementById("leave-room-hat").classList.toggle("hidden");
+    document.getElementById("right").classList.toggle("hidden");
+    userInputs.push(input);
+  } else if (input == "cafeteria" && userInputs.includes("no")) {
     document.getElementById("leave-room-no-hat").classList.toggle("hidden");
     document.getElementById("left").classList.toggle("hidden");
-  } else if (input == "electrical") {
+    userInputs.push(input);
+  } else if (input == "electrical" && userInputs.includes("no")) {
     document.getElementById("leave-room-no-hat").classList.toggle("hidden");
     document.getElementById("right").classList.toggle("hidden");
-  } else if (input == "left") {
+    userInputs.push(input);
+  } else if (input == "left" && userInputs.includes("exit")) {
     document.getElementById("exit-room").classList.toggle("hidden");
     document.getElementById("left").classList.toggle("hidden");
-  } else if (input == "back") {
+    userInputs.push(input);
+  } else if (
+    input == "back" &&
+    userInputs.includes("exit" || "left" || "cafeteria" || "east")
+  ) {
     document.getElementById("left").classList.toggle("hidden");
     document.getElementById("right").classList.toggle("hidden");
-  } else if (input == "right") {
+    userInputs.push(input);
+  } else if (input == "right" && userInputs.includes("exit")) {
     document.getElementById("exit-room").classList.toggle("hidden");
     document.getElementById("right").classList.toggle("hidden");
-  } else if (input == "restart") {
+    userInputs.push(input);
+  } else if (
+    input == "restart" &&
+    userInputs.includes("exit" || "electrical" || "right")
+  ) {
     document.getElementById("right").classList.toggle("hidden");
     document.getElementById("intro").classList.toggle("hidden");
   } else {
